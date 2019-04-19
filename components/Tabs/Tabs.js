84f241789a0a37b4319a521cc549f10d50d7ec95
Dominic Torruellas <dom@domtorr.com>
2019-04-18 20:39:31 -0700

    replaces using this.element.dataset.tab with this.data

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,7 +6,7 @@ class TabLink {
     this.data = element.dataset.tab;
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(
-      `.tabs-item[data-tab='${element.dataset.tab}']`
+      `.tabs-item[data-tab='${this.data}']`
     );
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
@@ -62,5 +62,3 @@ class TabItem {
 links = document
   .querySelectorAll('.tabs-link')
   .forEach(element => new TabLink(element));
-
-// mvp complete
